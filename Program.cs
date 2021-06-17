d0c783aa517cdd2021bc5fd866e2763a92e85285
Robbie <Robbie@DESKTOP-UAEN14H>
2021-06-17 20:03:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassE1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square[] squares = new Square[10];
+            for(int i =0; i<10; i++)
+            {
+                squares[i] = new Square(i+1);
+                Console.WriteLine("{0}, {1}", squares[i].Side, squares[i].Area);
+            }
+        }
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassE1
+{
+    class Square
+    {
+        private int side;
+        private int area;
+
+        public Square(int side)
+        {
+            this.side = side;
+            calcArea();
+        }
+        private void calcArea()
+        {
+            this.area = side * side;
+        }
+        public int Side
+        {
+            get
+            {
+                return side;
+            }
+        }
+        public int Area
+        {
+            get
+            {
+                return area;
+            }
+        }
+    }
+}
